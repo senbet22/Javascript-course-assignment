d38d8653e0976aad19f2cbd9dfc92a49974e1457
senbet22 <152603405+senbet22@users.noreply.github.com>
2024-04-17 11:08:19 +0200

    v2
    Made some adjustments to Homepage and started on MensJacket

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function fetchProducts(url) {
   let data = await fetch(url);
   let products = await data.json();
   console.log(products);
-  // let products = response;
+
 
   const container = document.querySelector('.products');
   container.innerHTML = '';
@@ -16,45 +16,31 @@ async function fetchProducts(url) {
     const product = products[i];
 
     const container = document.querySelector('.products');
-    //     container.innerHTML = container.innerHTML + `<a href="3_specific_jacket.html" class="row">
-    //     <img src="images/model9.jpg" alt="">
-    //     <div class="product-text">
-    //         <h2>${product.title}</h2>
-    //     </div>
-
-    //     <div class="detail">
-    //         <h3><i class="fa-solid fa-check"></i> Waterproof and breathable</h3>
-    //         <h3><i class="fa-solid fa-check"></i> Lightweight and packable</h3>
-    //         <h3><i class="fa-solid fa-check"></i> advanced wind-resistant technology</h3>
-    //         <div class="color color-white"></div>
-    //         <div class="color color-grey"></div>
-    //         <div class="color color-darkblue"></div>
-    //         <p>$169.00</p>
-    //     </div>
-    // </a>`;
 
     const template = document.querySelector('#product');
     const pr = template.content.cloneNode(true);
 
     pr.querySelector('h2').textContent = product.title;
     if (product.onSale) {
-      pr.querySelector('.detail p').innerHTML = `<del>${product.price}</del>${product.discountedPrice}`;
+      pr.querySelector('.detail p').innerHTML = `<del>${product.price}</del> ${product.discountedPrice}`;
     } else {
       pr.querySelector('.detail p').textContent = product.price;
 
     }
 
+    /* Add a function changeColor that makes new price Red */
+
+    /* Imports images from API to the clone div*/
+    pr.querySelector('#jacket-img').src = product.image;
+
+    pr.querySelector('h3').textContent = product.description;
+    /* Appends alle the code above to HTML. */
     container.appendChild(pr);
 
   }
-
-
 };
 fetchProducts('https://api.noroff.dev/api/v1/rainy-days');
 
-// i++
-// i=i+1;
-//fetchProducts(PRODUCTS_URL)
 
 
 
